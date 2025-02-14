ad062201f00f0c575f5184eda40a1135298fa1a7
laksatechno <laksatechno@gmail.com>
2025-02-14 13:49:38 +0700

    update button selesai penawaran

--- sales-app/resources/js/custom.js
+++
@@ -1,8 +0,0 @@
-$(document).on("click", ".access-failed", function() {
-    swal.fire({
-        title: "Maaf!",
-        text: "Anda tidak memiliki akses!",
-        icon: "info",
-        timer: 2000,
-    });
-});
